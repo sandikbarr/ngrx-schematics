8fd476b2e3cd799f46668b00533fabe4f7c6b3bf
Sandi Barr <sandi.barr@prx.org>
2018-06-15 16:41:25 -0500

    gen item reducer

--- src/app/item/item.module.ts
+++ src/app/item/item.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EffectsModule } from '@ngrx/effects';
 import { ItemEffects } from '../effects/item.effects';
+import { StoreModule } from '@ngrx/store';
+import * as fromItem from '../reducers/item.reducer';
 
 @NgModule({
   imports: [
     CommonModule,
-    EffectsModule.forFeature([ItemEffects])
+    EffectsModule.forFeature([ItemEffects]),
+    StoreModule.forFeature('item', fromItem.reducer)
   ],
   declarations: []
 })
--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -6,13 +6,16 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
+import * as fromItem from './item.reducer';
 
 export interface State {
 
+  item: fromItem.State;
 }
 
 export const reducers: ActionReducerMap<State> = {
 
+  item: fromItem.reducer,
 };
 
 
